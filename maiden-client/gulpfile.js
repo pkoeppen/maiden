5eccfca4359c6c1725674bddc0c98bdc5ce42fd2
Peter Koeppen <p.hartzog.koeppen@gmail.com>
2018-11-22 11:32:44 +0100

    changed tabs to spaces

--- maiden-client/gulpfile.js
+++ maiden-client/gulpfile.js
@@ -26,7 +26,7 @@ gulp.task("hugo", (cb) => { buildSite(cb); });
 gulp.task("build", ["sass", "js", "fonts", "images"], (cb) => buildSite(cb, [], "production"));
 gulp.task("build-clean", ["clean"], () => gulp.start("build"));
 // gulp.task("dist", ["build-clean"], () => {
-// 	return gulp.src("dist/*")
+//   return gulp.src("dist/*")
 //         .pipe(zip("dist.zip"))
 //         .pipe(gulp.dest("dist"))
 // });
@@ -38,12 +38,12 @@ gulp.task("server", ["hugo", "sass", "js", "fonts", "images"], (cb) => runServer
 // Compile Sass to CSS and minify.
 
 gulp.task("sass", () => {
-	return gulp.src("src/sass/style.sass")
-	.pipe(sass())
-	.pipe(minCSS())
-	.pipe(rename({ suffix: ".min" }))
-	.pipe(gulp.dest("dist/css"))
-	.pipe(browserSync.stream());
+  return gulp.src("src/sass/style.sass")
+  .pipe(sass())
+  .pipe(minCSS())
+  .pipe(rename({ suffix: ".min" }))
+  .pipe(gulp.dest("dist/css"))
+  .pipe(browserSync.stream());
 });
 
 // Minify JS and copy to /dist.
@@ -52,40 +52,40 @@ gulp.task("js", () => {
 
   // Don't re-minify minified files...
 
-	gulp.src(["src/js/**/*", "!src/js/main.js", "!src/js/hero-background.js"])
-	.pipe(gulp.dest("dist/js"))
-	.pipe(browserSync.stream());
+  gulp.src(["src/js/**/*", "!src/js/main.js", "!src/js/hero-background.js"])
+  .pipe(gulp.dest("dist/js"))
+  .pipe(browserSync.stream());
 
   // ...only these ones.
 
-	return gulp.src(["src/js/main.js", "src/js/hero-background.js"])
-	.pipe(uglify())
-	.on("error", function(error) { console.log(error.toString()); })
-	.pipe(rename({ suffix: ".min" }))
-	.pipe(gulp.dest("dist/js"));
+  return gulp.src(["src/js/main.js", "src/js/hero-background.js"])
+  .pipe(uglify())
+  .on("error", function(error) { console.log(error.toString()); })
+  .pipe(rename({ suffix: ".min" }))
+  .pipe(gulp.dest("dist/js"));
 });
 
 // Copy fonts to /dist.
 
 gulp.task("fonts", () => {
-	return gulp.src("src/fonts/**/*") // TODO: flatten
-	.pipe(gulp.dest("dist/fonts"))
-	.pipe(browserSync.stream());
+  return gulp.src("src/fonts/**/*") // TODO: flatten
+  .pipe(gulp.dest("dist/fonts"))
+  .pipe(browserSync.stream());
 });
 
 // Copy images to /dist.
 
 gulp.task("images", () => {
-	return gulp.src("src/images/**/*") // TODO: minify images
-	.pipe(gulp.dest("dist/images"))
-	.pipe(browserSync.stream());
+  return gulp.src("src/images/**/*") // TODO: minify images
+  .pipe(gulp.dest("dist/images"))
+  .pipe(browserSync.stream());
 });
 
 gulp.task("html", ["clean-hugo"], function() {
-	return gulp.src("dist/*.html")
-	.pipe(htmlmin({collapseWhitespace: true}))
-	.pipe(gulp.dest("dist"))
-	.pipe(browserSync.stream());
+  return gulp.src("dist/*.html")
+  .pipe(htmlmin({collapseWhitespace: true}))
+  .pipe(gulp.dest("dist"))
+  .pipe(browserSync.stream());
 });
 
 gulp.task("clean", function () {
@@ -105,35 +105,35 @@ gulp.task("clean-hugo", function () {
 });
 
 function runServer() {
-	browserSync.init({
-		server: {
-			baseDir: "./dist"
-		}
-	});
-	gulp.watch("src/js/**/*.js", ["js"]);
-	gulp.watch("src/sass/**/*.sass", ["sass"]);
-	gulp.watch("src/fonts/**/*", ["fonts"]);
-	gulp.watch("src/images/**/*", ["images"]);
-	gulp.watch("site/**/*", ["hugo"]);
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  });
+  gulp.watch("src/js/**/*.js", ["js"]);
+  gulp.watch("src/sass/**/*.sass", ["sass"]);
+  gulp.watch("src/fonts/**/*", ["fonts"]);
+  gulp.watch("src/images/**/*", ["images"]);
+  gulp.watch("site/**/*", ["hugo"]);
 }
 
 function buildSite(cb, options, environment="development") {
-	const args = options ? defaultHugoArgs.concat(options) : defaultHugoArgs;
+  const args = options ? defaultHugoArgs.concat(options) : defaultHugoArgs;
 
-	process.env.NODE_ENV = environment;
+  process.env.NODE_ENV = environment;
 
-	return child.spawn(hugobin, args, {stdio: "inherit"}).on("close", (status) => {
-		if (status === 0) {
+  return child.spawn(hugobin, args, {stdio: "inherit"}).on("close", (status) => {
+    if (status === 0) {
 
-			// Minify HTML after successful Hugo build.
+      // Minify HTML after successful Hugo build.
 
-			gulp.start("html"); // gulp.start() will be deprecated someday
+      gulp.start("html"); // gulp.start() will be deprecated someday
 
-			cb();
+      cb();
 
-		} else {
-			browserSync.notify("Hugo build failed.");
-			cb("Hugo build failed.");
-		}
-	});
+    } else {
+      browserSync.notify("Hugo build failed.");
+      cb("Hugo build failed.");
+    }
+  });
 }
